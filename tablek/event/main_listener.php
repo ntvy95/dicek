88311914fd262c6ec909242a59ed5426943ae87f
ntvy95 <ntvy95@gmail.com>
2015-07-08 22:47:09 +0700

    Tablek: More advanced version. Only need some modifications.

--- tablek/event/main_listener.php
+++ tablek/event/main_listener.php
@@ -40,78 +40,82 @@ class main_listener implements EventSubscriberInterface
 	}
 	
 	static public function tablek_parse($message) {
-			$open_tag = "[tablek]";
+			$open_tag_start = "[tablek";
+			$open_tag_end = "]";
 			$close_tag = "[/tablek]";
-			$isopen = strpos($message, $open_tag, 0);
+			$isopen = strpos($message, $open_tag_start, 0);
 			while($isopen !== false) {
-				$isopen = $isopen + strlen($open_tag);
-				//$isopen = $open_matches[0][1] + strlen($open_matches[0][0]);
-				$isclose = strpos($message, $close_tag, $isopen2);
-				//exit(var_dump($isclose));
-				if($isclose !== false) {
-					$table = substr($message, $isopen, $isclose - $isopen);
-					//var_dump($table);
-					/*$table_tag = explode("|", $open_matches[0][0], 2);
-					$table_tag_html = $table_tag[0];
-					if(empty($table_tag_html) == false) {
-						//BEGIN: Table HTML attributes.
-						preg_match(self::$pattern_id, $table_tag_html, $table_id);
-						preg_match(self::$pattern_class, $table_tag_html, $table_class);
-						//END: Table HTML attributes.
+				$isopen = $isopen + strlen($open_tag_start);
+				$isopen2 = strpos($message, $open_tag_end, $isopen);
+				if($isopen2 !== false) {
+					$table_tag = substr($message, $isopen, $isopen2 - $isopen);
+					$isopen2 = $isopen2 + strlen($open_tag_end);
+					$isclose = strpos($message, $close_tag, $isopen2);
+					if($isclose !== false) {
+						$table = substr($message, $isopen2, $isclose - $isopen2);
+						if(empty($table_tag) == false) {
+							$table_tag = explode("|", $table_tag, 2);
+							$table_tag_html = $table_tag[0];
+							if(empty($table_tag_html) == false) {
+								//BEGIN: Table HTML attributes.
+								preg_match(self::$pattern_id, $table_tag_html, $table_id);
+								preg_match(self::$pattern_class, $table_tag_html, $table_class);
+								//END: Table HTML attributes.
+							}
+							$table_tag_css = '';
+							if(isset($table_tag[1])) {
+								$table_tag_css = $table_tag[1];
+							}
+							$head = '<table id="' . $table_id[1] . '" class=' . $table_class[1] . ' style="' . $table_tag_css . '">';
+						}
+						else {
+							$head = '<table>';
+						}
+						$body = $table;
+						$tail = '</table>';
+						$rows = preg_split(self::$pattern_row_overall, $body, NULL, PREG_SPLIT_OFFSET_CAPTURE);
+						for($index = 1, $index_end = sizeof($rows); $index < $index_end; $index++){
+							$row = $rows[$index];
+							$begin_tag = strpos($body, '{', intval($rows[$index - 1][1]));
+							$end_tag = strpos($body, '}', $begin_tag);
+							if($end_tag === false) {
+								unset($rows[$index]);
+								continue;
+							}
+								
+							$row_tag = substr($body, $begin_tag, $end_tag - $begin_tag);
+							$row_tag = explode("|", $row_tag, 2);
+							$row_tag_html = $row_tag[0];
+							if(empty($row_tag_html) == false) {
+								//BEGIN: Row HTML attributes.
+								preg_match(self::$pattern_id, $row_tag_html, $row_id);
+								preg_match(self::$pattern_class, $row_tag_html, $row_class);
+								//END: Row HTML attributes.
+							}
+							$row_tag_css = '';
+							if(isset($row_tag[1])) {
+								$row_tag_css = $row_tag[1];
+							}
+								
+							$head_row = '<tr id=' . $row_id[1] . ' class=' . $row_class[1] . ' style="' . $row_tag_css . '" />';
+							$body_row = self::row_parse($row[0]);
+							if(strcmp($body_row, $row[0]) === 0) {
+								$body_row = '';
+							}
+							$tail_row = '</tr>';
+								
+							$rows[$index] = $head_row . $body_row . $tail_row;				
+						}
+						unset($rows[0]);
+						$body = implode($rows);
+						$table = $head . $body . $tail;
+						$isclose = $isclose + strlen($close_tag);
+						$message = substr_replace($message, $table, $isopen - strlen($open_tag_start), $isclose + strlen($close_tag) - $isopen);
 					}
-					$table_tag_css = '';
-					if(isset($table_tag[1])) {
-						$table_tag_css = $table_tag[1];
-					}*/
-					//$head = '<table id="' . $table_id[1] . '" class ="' . $table_class[1] . '" style="' . $table_tag_css . '">';
-					$head = '<table>';
-					$body = $table;
-					//var_dump($body);
-					$tail = '</table>';
-					$rows = preg_split(self::$pattern_row_overall, $body, NULL, PREG_SPLIT_OFFSET_CAPTURE);
-					for($index = 1, $index_end = sizeof($rows); $index < $index_end; $index++){
-						$row = $rows[$index];
-						$begin_tag = strpos($body, '{', intval($rows[$index - 1][1]));
-						$end_tag = strpos($body, '}', $begin_tag);
-						if($end_tag === false) {
-							unset($rows[$index]);
-							continue;
-						}
-							
-						$row_tag = substr($body, $begin_tag, $end_tag - $begin_tag);
-						$row_tag = explode("|", $row_tag, 2);
-						$row_tag_html = $row_tag[0];
-						if(empty($row_tag_html) == false) {
-							//BEGIN: Row HTML attributes.
-							preg_match(self::$pattern_id, $row_tag_html, $row_id);
-							preg_match(self::$pattern_class, $row_tag_html, $row_class);
-							//END: Row HTML attributes.
-						}
-						$row_tag_css = '';
-						if(isset($row_tag[1])) {
-							$row_tag_css = $row_tag[1];
-						}
-							
-						$head_row = '<tr id=' . $row_id[1] . ' class=' . $row_class[1] . ' style="' . $row_tag_css . '" />';
-						$body_row = self::row_parse($row[0]);
-						if(strcmp($body_row, $row[0]) === 0) {
-							$body_row = '';
-						}
-						$tail_row = '</tr>';
-							
-						$rows[$index] = $head_row . $body_row . $tail_row;				
-					}
-					unset($rows[0]);
-					$body = implode($rows);
-					$table = $head . $body . $tail;
-					$isclose = $isclose + strlen($close_tag);
-					$message = substr_replace($message, $table, $isopen - strlen($open_tag), $isclose + strlen($close_tag) - $isopen);
 				}
-				$isopen = strpos($message, $open_tag, $isclose);
+				$isopen = strpos($message, $open_tag_start, $isclose);
 			}
 			return $message;
-		//}
-		//return preg_replace_callback(self::$pattern_table, 'self::tablek_parse', $message);
 	}
 	
 	static public function row_parse($row) {
