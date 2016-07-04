12ecee9b5d3e5112ebe5cdea03d8a63d9b33d433
ntvy95 <ntvy95@gmail.com>
2016-07-04 12:10:07 +0700

    Update dicek bbcode.

--- dicek/event/main_listener.php
+++ dicek/event/main_listener.php
@@ -15,27 +15,19 @@ class main_listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-			//'core.posting_modify_submit_post_before' => 'posting_modify_submit_post_before',
-			//'core.posting_modify_submit_post_after' => 'posting_modify_submit_post_after',
-			//'core.posting_modify_message_text' => 'posting_modify_message_text',
 			'core.submit_post_end' => 'submit_post_end',
-			'core.viewtopic_modify_post_data' => 'viewtopic_modify_post_data',
-			'core.modify_posting_auth' => 'modify_posting_auth',
-			'core.modify_format_display_text_after' => 'modify_format_display_text_after',
+			'core.viewtopic_post_rowset_data'	=> 'viewtopic_post_rowset_data',
+			'core.topic_review_modify_row' => 'topic_review_modify_row',
 		);
 	}
 	protected $db;
 	protected $posts_table;
-	protected $post_id;
-	protected $dicek_range_index;
-	protected $post_dicek;
 	static $pattern;
 	static $pattern_checkdicek;
 	
 	public function __construct(\phpbb\db\driver\driver_interface $db, $posts_table) {
 		$this->db = $db;
 		$this->posts_table = $posts_table;
-		$this->dicek_range_index = array();
 		self::$pattern = '@\[dicek\]([0-9]+|([0-9]+(-[0-9]+)+))\[/dicek\]@i';
 		self::$pattern_checkdicek = '@\[checkdicek](|[0-9]+)\[/checkdicek]@i';
 	}
@@ -43,36 +35,59 @@ class main_listener implements EventSubscriberInterface
 	public function submit_post_end($event) {
 		$message = $event['data']['message'];
 		$post_id = $event['data']['post_id'];
-		$dicek_range = self::get_dicek_range_from_message($message);
+		$dicek_range = $this->get_dicek_range_from_message($message);
 
-		if(empty($dicek_range) == false) {
-
-			$current_dicek_range_string = $this->get_current_dicek_range_string($post_id);
-			$current_dicek_range = self::convert_dicek_string_to_array($current_dicek_range_string);
-			$new_dicek_range = self::new_dicek_range($current_dicek_range, $dicek_range);
-			$new_dicek_value = self::generate_dicek_value($new_dicek_range);
-			
-			$new_dicek_range_string = self::convert_dicek_array_to_string($new_dicek_range);
-			$new_dicek_value_string = self::convert_dicek_array_to_string($new_dicek_value);
-			
-			if(empty($current_dicek_range_string) == false) {
-				$new_dicek_range_string = '-' . $new_dicek_range_string;
-				$new_dicek_value_string = '-' . $new_dicek_value_string;
+		if(!empty($dicek_range)) {			
+			$current_dicek_range_string = $this->get_current_dicek_range_string($post_id);			
+			$current_dicek_range = $this->convert_dicek_string_to_array($current_dicek_range_string);
+			$new_dicek_range = $this->new_dicek_range($current_dicek_range, $dicek_range);
+			if(count($new_dicek_range) > 0) { 
+				$new_dicek_value = $this->generate_dicek_value($new_dicek_range);
+					
+				$new_dicek_range_string = $this->convert_dicek_array_to_string($new_dicek_range);
+				$new_dicek_value_string = $this->convert_dicek_array_to_string($new_dicek_value);
+				if(!empty($current_dicek_range_string)) {
+					$new_dicek_range_string = '-' . $new_dicek_range_string;
+					$new_dicek_value_string = '-' . $new_dicek_value_string;
+				}					
+				$current_dicek_value_string = $this->get_current_dicek_value_string($post_id);
+					
+				$dicek_range_string = $current_dicek_range_string . $new_dicek_range_string;
+				$dicek_value_string = $current_dicek_value_string . $new_dicek_value_string;
+					
+				$sql_update = 'UPDATE ' . $this->posts_table . '
+							SET dicek_range = "' . $dicek_range_string . '", dicek_value = "' . $dicek_value_string . '"
+							WHERE post_id = ' . (int) $post_id;
+				$this->db->sql_query($sql_update);
 			}
-			
-			$current_dicek_value_string = $this->get_current_dicek_value_string($post_id);
-			
-			$dicek_range_string = $current_dicek_range_string . $new_dicek_range_string;
-			$dicek_value_string = $current_dicek_value_string . $new_dicek_value_string;
-			
-			$sql_update = 'UPDATE ' . $this->posts_table . '
-						SET dicek_range = "' . $dicek_range_string . '", dicek_value = "' . $dicek_value_string . '"
-						WHERE post_id = ' . (int) $post_id;
-			$this->db->sql_query($sql_update);
 		}
 	}
 	
-	static public function generate_dicek_value($dicek_range) {
+	public function viewtopic_post_rowset_data($event) {
+		$rowset_data = $event['rowset_data'];
+		$rowset_data['post_text'] = $this->parse_bbcode_dicek($rowset_data['post_id'], $rowset_data['post_text']);
+		$event['rowset_data'] = $rowset_data;
+	}
+	
+	public function topic_review_modify_row($event) {
+		$post_row = $event['post_row'];
+		$row = $event['row'];		
+		$post_row['MESSAGE'] = $this->parse_bbcode_dicek($row['post_id'], $post_row['MESSAGE']);
+		$event['post_row'] = $post_row;
+	}
+	
+	public function parse_bbcode_dicek($post_id, $message) {
+		$post_dicek_range_string = $this->get_current_dicek_range_string($post_id);
+		if(empty($post_dicek_range_string) === false) {
+			$post_dicek_value_string = $this->get_current_dicek_value_string($post_id);
+			$post_dicek = $this->get_current_dicek($post_dicek_range_string, $post_dicek_value_string);
+			$message = $this->message_dicek_after($message, $post_dicek, $post_id);
+		}
+		$message = $this->message_checkdicek_after($message, $post_id);
+		return $message;
+	}
+	
+	public function generate_dicek_value($dicek_range) {
 		$dicek_value = array();
 		foreach($dicek_range as $index => $max) {
 			$dicek_value[$index] = rand(1, intval($max));
@@ -80,12 +95,12 @@ class main_listener implements EventSubscriberInterface
 		return $dicek_value;
 	}
 	
-	static public function new_dicek_range($current, $posting) {
+	public function new_dicek_range($current, $posting) {
 		$new_dicek_range = array();
-		$current_count = self::count_repeat_value($current);
-		$posting_count = self::count_repeat_value($posting);
+		$current_count = $this->count_repeat_value($current);
+		$posting_count = $this->count_repeat_value($posting);
 		foreach($posting as $value) {
-			if(in_array($value, $current, true) == false || $posting_count[$value] > $current_count[$value]) {
+			if(!in_array($value, $current, true) || $posting_count[$value] > $current_count[$value]) {
 				array_push($new_dicek_range, $value);
 				$posting_count[$value] = $posting_count[$value] - 1;
 			}
@@ -93,10 +108,10 @@ class main_listener implements EventSubscriberInterface
 		return $new_dicek_range;
 	}
 	
-	static public function count_repeat_value($arr) {
+	public function count_repeat_value($arr) {
 		$count = array();
 		foreach($arr as $value) {
-			if(array_key_exists($value, $count) == false) {
+			if(!array_key_exists($value, $count)) {
 				$count[$value] = 0;
 			}
 			$count[$value] = $count[$value] + 1;
@@ -104,11 +119,11 @@ class main_listener implements EventSubscriberInterface
 		return $count;
 	}
 	
-	static public function get_dicek_range_from_message($message) {
+	public function get_dicek_range_from_message($message) {
 		preg_match_all(self::$pattern, $message, $matches);
 		$dicek_range = array();
 		foreach($matches[1] as $match) {
-			$match_arr = self::convert_dicek_string_to_array($match);
+			$match_arr = $this->convert_dicek_string_to_array($match);
 			$dicek_range = array_merge($dicek_range, $match_arr);
 		}
 		return $dicek_range;
@@ -134,25 +149,25 @@ class main_listener implements EventSubscriberInterface
 		return $data['dicek_value'];
 	}
 	
-	static public function convert_dicek_string_to_array($string) {
+	public function convert_dicek_string_to_array($string) {
 		if(empty($string) == false) {
 			return explode('-', $string);
 		}
 		return array();
 	}
 	
-	static public function convert_dicek_array_to_string($arr) {
+	public function convert_dicek_array_to_string($arr) {
 		return implode('-', $arr);
 	}
 	
-	static public function get_current_dicek($dicek_range_string, $dicek_value_string) {
-		$dicek_range = self::convert_dicek_string_to_array($dicek_range_string);
-		$dicek_value = self::convert_dicek_string_to_array($dicek_value_string);
-		$dicek = self::key_to_data($dicek_range, $dicek_value);
+	public function get_current_dicek($dicek_range_string, $dicek_value_string) {
+		$dicek_range = $this->convert_dicek_string_to_array($dicek_range_string);
+		$dicek_value = $this->convert_dicek_string_to_array($dicek_value_string);
+		$dicek = $this->key_to_data($dicek_range, $dicek_value);
 		return $dicek;
 	}
 	
-	static public function key_to_data($key, $data) {
+	public function key_to_data($key, $data) {
 		$arr = array();
 		foreach($data as $index => $value) {
 			if($arr[$key[$index]] == NULL) {
@@ -163,73 +178,41 @@ class main_listener implements EventSubscriberInterface
 		return $arr;
 	}
 	
-	public function message_dicek_after($message) {
+	public function message_dicek_after($message, $post_dicek, $post_id) {
+		$dicek_range_index = array();
 		return preg_replace_callback(self::$pattern,
-		function ($match) {
-			return $this->bb_dicek_replace($match[1]);
+		function ($match) use ($post_dicek, $post_id, &$dicek_range_index) {
+			return $this->bb_dicek_replace($match[1], $dicek_range_index, $post_dicek, $post_id);
 		}, $message);
 	}	
 	
-	public function message_checkdicek_after($message) {
+	public function message_checkdicek_after($message, $current_post_id) {
 		return preg_replace_callback(self::$pattern_checkdicek,
-		function ($match) {
-			return $this->bb_checkdicek_replace($match[1]);
+		function ($match) use ($current_post_id) {
+			return $this->bb_checkdicek_replace($match[1], $current_post_id);
 		}, $message);
 	}
 	
-	public function bb_dicek_replace($dicek_range_string) {
-			$dicek_range = self::convert_dicek_string_to_array($dicek_range_string);
-				$dicek_value = array();
-				foreach($dicek_range as $range) {
-					if(array_key_exists($range, $this->dicek_range_index) == false) {
-						$this->dicek_range_index[$range] = 0;
-					}
-					array_push($dicek_value, $this->post_dicek[$range][$this->dicek_range_index[$range]]);
-					$this->dicek_range_index[$range] = $this->dicek_range_index[$range] + 1;
-				}
-				return '<a target="_blank" href="checkdicek.php?post_id=' . $this->post_id . '">' . self::convert_dicek_array_to_string($dicek_value) . '</a>';
+	public function bb_dicek_replace($dicek_range_string, &$dicek_range_index, $post_dicek, $post_id) {
+		$dicek_range = $this->convert_dicek_string_to_array($dicek_range_string);
+		$dicek_value = array();
+		foreach($dicek_range as $range) {
+			if(array_key_exists($range, $dicek_range_index) == false) {
+				$dicek_range_index[$range] = 0;
+			}
+			array_push($dicek_value, $post_dicek[$range][$dicek_range_index[$range]]);
+			$dicek_range_index[$range] = $dicek_range_index[$range] + 1;
+		}
+		return '<a target="_blank" href="checkdicek.php?post_id=' . $post_id . '">' . $this->convert_dicek_array_to_string($dicek_value) . '</a>';
 	}
 	
-	public function bb_checkdicek_replace($post_id) {
+	public function bb_checkdicek_replace($post_id, $current_post_id) {
 		if(empty($post_id)) {
-			if($this->post_id != NULL) {
-				$post_id = $this->post_id;
-			}
-			else {
-				return '[checkdicek][/checkdicek]';
-			}
+			$post_id = $current_post_id;
 		}
 		$message = '<hr />POST ID: ' . $post_id . '<br /> Dice Range: ' .
-					self::get_current_dicek_range_string($post_id) .
-					'<br /> Corresponding Dice Result: ' . self::get_current_dicek_value_string($post_id) . '<hr />';
+					$this->get_current_dicek_range_string($post_id) .
+					'<br /> Corresponding Dice Result: ' . $this->get_current_dicek_value_string($post_id) . '<hr />';
 		return $message;
-	}
-	
-	public function viewtopic_modify_post_data($event) {
-		$rowset = $event['rowset'];
-		for($i = 0, $size = sizeof($event['post_list']); $i < $size; $i++) {
-			$this->post_id = $event['post_list'][$i];
-			$message = $rowset[$this->post_id]['post_text'];
-			$post_dicek_range_string = $this->get_current_dicek_range_string($this->post_id);
-			if(empty($post_dicek_range_string) === false) {
-				$post_dicek_value_string = $this->get_current_dicek_value_string($this->post_id);
-				$this->post_dicek = self::get_current_dicek($post_dicek_range_string, $post_dicek_value_string);
-				$message = $this->message_dicek_after($message);
-				$this->dicek_range_index = array();
-			}
-			$message = $this->message_checkdicek_after($message);
-			$rowset[$this->post_id]['post_text'] = $message;
-		}
-		$event['rowset'] = $rowset;
-	}
-	
-	public function modify_format_display_text_after($event) {
-		$event['text'] = $this->message_checkdicek_after($event['text']);
-	}
-	
-	public function modify_posting_auth($event) {
-		if(empty($event['post_id']) == false) {
-			$this->post_id = $event['post_id'];
-		}
 	}
 }
